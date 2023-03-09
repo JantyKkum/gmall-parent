83224cda4a4e9d049ebef248e9a04079cb7c2982
janty <billjian.bj@gmail.com>
2023-03-09 22:02:25 +0800

    SPU销售属性及图例列表查询实现

---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/controller/SkuManageController.java
@@ -0,0 +1,64 @@
+package com.atguigu.gmall.product.controller;
+
+import com.atguigu.gmall.common.result.Result;
+import com.atguigu.gmall.model.product.SkuInfo;
+import com.atguigu.gmall.model.product.SpuImage;
+import com.atguigu.gmall.model.product.SpuSaleAttr;
+import com.atguigu.gmall.product.service.ManageService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author: Janty
+ * @projectName: gmall-parent
+ * @date: 2023/3/9 21:00
+ * @description:
+ */
+@Api(tags = "商品SKU接口")
+@RestController
+@RequestMapping("admin/product")
+public class SkuManageController {
+
+    @Autowired
+    private ManageService manageService;
+
+    /**
+     * 根据spuId 查询（销售属性和销售属性值）集合
+     * @param spuId
+     * @return
+     */
+    @GetMapping("spuSaleAttrList/{spuId}")
+    public Result<List<SpuSaleAttr>> getSpuSaleAttrList(@PathVariable("spuId") Long spuId) {
+        List<SpuSaleAttr> spuSaleAttrList = manageService.getSpuSaleAttrList(spuId);
+        return Result.ok(spuSaleAttrList);
+    }
+
+
+    /**
+     * 根据spuId 查询spuImageList
+     * @param spuId
+     * @return
+     */
+    @GetMapping("spuImageList/{spuId}")
+    public Result<List<SpuImage>> getSpuImageList(@PathVariable("spuId") Long spuId) {
+        List<SpuImage> spuImageList = manageService.getSpuImageList(spuId);
+        return Result.ok(spuImageList);
+    }
+
+//    /**
+//     * 保存sku
+//     * @param skuInfo
+//     * @return
+//     */
+//    @PostMapping("/saveSkuInfo")
+//    public Result saveSkuInfo(@RequestBody SkuInfo skuInfo) {
+//        // 调用服务层
+//        manageService.saveSkuInfo(skuInfo);
+//        return Result.ok();
+//    }
+
+}
+
