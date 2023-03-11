7a4eaf99a6586048a223699e9dc1703677c1d1e3
janty <billjian.bj@gmail.com>
2023-03-11 23:17:21 +0800

    搭建service-item

---
+++ service/service-item/src/main/java/com/atguigu/gmall/item/service/ItemService.java
@@ -0,0 +1,20 @@
+package com.atguigu.gmall.item.service;
+
+import java.util.Map;
+
+/**
+ * @author: Janty
+ * @projectName: gmall-parent
+ * @date: 2023/3/11 23:11
+ * @description:
+ */
+public interface ItemService {
+
+    /**
+     * 获取sku详情信息
+     * @param skuId
+     * @return
+     */
+    Map<String, Object> getBySkuId(Long skuId);
+}
+
